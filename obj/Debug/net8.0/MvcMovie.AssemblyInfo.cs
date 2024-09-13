45e51799b93cceba964c0e5d1f2a31e8e36f41df
craigology <ibenicraig@gmail.com>
2024-09-13 18:30:18 +0300

    Add Ratings field

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76f5fd3fb2497bd81caee14a7a872665b1b14a53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70c9e2daec1724d9ef8a45db90f0224b22de79e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
