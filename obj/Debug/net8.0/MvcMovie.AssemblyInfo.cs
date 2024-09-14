d13f8fa1cc0d88a63584e96ea80eca54849be4bb
craigology <ibenicraig@gmail.com>
2024-09-14 16:50:53 +0300

    Add Rating field and make db migration

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6f8732f9ad77ce784cfc5936a25e76af398e68e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+342b23ad1939ed7e6ddde525c5ac72ca362d8e11")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
