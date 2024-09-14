0ac61e1e48ddadb1d0b2e21c79f2dc3c743d2318
craigology <ibenicraig@gmail.com>
2024-09-14 15:41:21 +0300

    Add assembly info bs (bis)

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cb88f4fcf31e8c2b9fba3055f0083f99274a2a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+373b236af7d422f435bd1a9ddb5e5659d2aa76e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
